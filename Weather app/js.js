fc11389c5f902ae41b8a3a1efa8db1259b1d991f
Hannah Cassius <hannahcassius@gmail.com>
2015-11-16 21:16:06 -0500

    updates
    to weather app

--- Weather app/js.js
+++ Weather app/js.js
@@ -1,4 +1,3 @@
-http://api.openweathermap.org/data/2.5/weather?q=<CITYNAME>
 
 $(function () {
 
@@ -13,7 +12,7 @@ $(function () {
 
 		console.log("The city is " + city);
 
-		var url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + encodeURIComponent(city) + "&cnt=4&mode=json";
+		var url = "http://api.openweathermap.org/data/2.5/weather?q=<CITYNAME>" ;
 
 		console.log("The url is " + url);
 
@@ -21,65 +20,10 @@ $(function () {
 			url: url,
 			dataType: "jsonp",
 			success: function (data) {
-				console.log(data);
-
-				var list = data.list;
-
-				processList(list);
-			}
+				console.log(data); }
+});
 		});
 
-
-
-		function processList(list) {
-			console.log(list);
-
-			var weatherData = [];
-
-			for (var i = 0; i < list.length; i++) {
-
-				var weatherDataObject = {
-					minTemp: kelvinToFahrenheit(list[i].temp.min).toFixed(0),
-					maxTemp: kelvinToFahrenheit(list[i].temp.max).toFixed(0),
-					dayOut: i,
-					dayName: getDayName(i)
-				};
-
-				weatherData.push(weatherDataObject);
-			}
-
-			console.log("The new weather data array ", weatherData);
-
-			createForecastElements(weatherData);
-		}
-
-
-		function getDayName(dayOut) {
-			var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-			var currentDate = new Date();
-
-			var date = new Date(currentDate);
-			date.setDate(currentDate.getDate() + dayOut);
-
-			return dayNames[date.getDay()];
-		}
-
-
-		function createForecastElements(weatherData) {
-			console.log("Weather data", weatherData);
-
-			$("#forecast").html("");
-
-			for (var i = 0; i < weatherData.length; i++) {
-				$("#forecast").append("" + 
-					"<div class='day-weather'>" +
-						"<div class='day'>" + weatherData[i].dayName + "</div>" +
-						"<div class='min-temp'>" + weatherData[i].minTemp + "</div>" +
-						"<div class='max-temp'>" + weatherData[i].maxTemp + "</div>" +
-					"</div>");	
-			}
-			// $("#forecast")
-
-		}
+})
 
  
